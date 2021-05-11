0a8e167a4bb4c06f5e6c69bfb2db60cf155aa531
diegomohammadbrazil <52060378+diegomohammadbrazil@users.noreply.github.com>
2021-05-11 19:49:17 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,5 @@
+def soma(a='Não sei dirigir', b='Não tenho CNH', c='Estou na autoescola'):
+    argumento = a + b + c
+    return(argumento)
+
+    print(f'Estes são os 3 argumentos: {soma(a + b + c)}')
---
+++ ex2.py
@@ -0,0 +1,11 @@
+def argumento(a=P, b=N):
+    P = [4, 5]
+    N = [4, 8]
+    if a int(input('Digite o argumento: '))
+    print(f'O argumento {P}')
+
+    else:
+         b = int(input('Digite o argumento: '))
+    print(f'O argumento {N}')
+
+
