4a717b83f6bde3037b5d9a596fb4574b8b4cb07f
diegomohammadbrazil <binorx@hotmail.com>
2021-05-12 22:21:42 -0300

    commit teste

--- exercicio2.py
+++ exercicio2.py
@@ -1,4 +1,11 @@
-n = int(input('Digite um num ao qual você quer dividir: '))
-for c in range(1, n+1):
-    if n % c --0:
-        print(f'{c}é um divisor de{n}') 
+num = (int(input('1º valor:')), int(input('2º valor:')), int(input('3º valor:')), int(input('4º valor:')))
+print(num)
+print(f'O número 9 aparece {num.count(9)} vezes.')
+if 3 in num:
+    print(f'O número 3 foi digitado pela primeira vez na posição {num.index(3)}')
+else:
+    print('O número 3 não foi digitado')
+print('Números pares digitados: ', end='')
+for i in num:
+    if i % 2 == 0:
+        print(i, end=' ')
